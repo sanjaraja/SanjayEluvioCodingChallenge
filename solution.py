715f6cdde6dbf9147c90ee24dc9742de7b3b7214
Sanjay Rajasekaran <rajasekaran.sanjay@gmail.com>
2021-04-20 15:52:33 -0700

    Cleaning up solution

--- solution.py
+++ solution.py
@@ -1,31 +1,26 @@
 from concurrent.futures import ThreadPoolExecutor
 import requests
 
+id_set = set()
 def fetch(session, url, id):
-    print("Entering fetch")
-    headers = {
-        "Authorization": "Y1JGMmR2RFpRc211MzdXR2dLNk1UY0w3WGpI"
-    }
-    url = url + id
-    print(url)
-    with session.get(url, headers=headers) as response:
-        print(response.status_code)
+    if id not in id_set:
+        print("Entering fetch")
+        headers = {
+            "Authorization": "Y1JGMmR2RFpRc211MzdXR2dLNk1UY0w3WGpI"
+        }
+        url = url + id
+        with session.get(url, headers=headers) as response:
+            print(response.status_code) #Printing the response status code. 
+    else:
+        print("This is a duplicate ID")
 
 if  __name__ == "__main__":
-    """
     ids = ["cRF2dvDZQsmu37WGgK6MTcL7XjH"]
-    for num in range(0, 20):
+    for num in range(0, 19): #Edit the range if you want more IDS
         ids.append(str(num))
 
     url = "https://eluv.io/items/"
-    with ThreadPoolExecutor(max_workers=5) as executor:
+    with ThreadPoolExecutor(max_workers=5) as executor: #Ensuring that there are only 5 threads at a time to avoid too many requests rerror
         with requests.Session() as session:
-            executor.map(fetch, [session] * 20, [url] * 20, ids)
+            executor.map(fetch, [session] * 20, [url] * 20, ids) #Change the 20 here if you want more IDs
             executor.shutdown(wait=True)
-    """
-    #fetch(requests.Session(), "https://eluv.io/items/", "cRF2dvDZQsmu37WGgK6MTcL7XjH")
-    headers_dict = {
-        "Authorization": "Y1JGMmR2RFpRc211MzdXR2dLNk1UY0w3WGpI"
-    }
-    r= requests.get("https://eluv.io/items/cRF2dvDZQsmu37WGgK6MTcL7XjH", headers=headers_dict)
-    print(r.status_code)
